b2c70269843ea2f07a2522d45a2ccfe13cdea783
Pedro Seiti <pedroseiti@hotmail.com>
2017-02-24 20:08:37 +0100

    Added DFT's calculations

--- amidar-sim2/Applications/src/gps/acquisition/Acquisition.java
+++ amidar-sim2/Applications/src/gps/acquisition/Acquisition.java
@@ -54,25 +54,27 @@ public class Acquisition {
 	
 	// Acquisition function and its auxiliary functions
 	public boolean startAcquisition(){
-	
+		float Pin;
+		
 		int menge = calculateMengeFrequenzen();
 		
-		float[][] realMatrix = new float[N][menge];
-		float[][] imagMatrix = new float[N][menge];
+		float[][] realXfd = new float[menge][N]; // Matrix Xfd (lines => frequencies; columns => samples)
+		float[][] imagXfd = new float[menge][N]; //
 		
-		float realEuler, imagEuler, Pin;
-		int n, fd, m = 0;
+		float[] realDftCode = new float[N];
+		float[] imagDftCode = new float[N];
 		
-		for(fd = minFrequenz; fd <= maxFrequenz; fd += stepFrequenz){
-			for(n = 0; n < N; n++){
-				realEuler = calculateRealEulerFactors(fd, n);
-				imagEuler = calculateImagEulerFactors(fd, n);
-				
-				realMatrix[n][m] = realSample[n]*realEuler - imagSample[n]*imagEuler;
-				imagMatrix[n][m] = imagSample[n]*realEuler - realSample[n]*imagEuler;
-			}
-			m++;
-		}
+		float[][] realMatrix = new float[menge][N]; // Final matrix (lines => frequencies; columns => samples)
+		float[][] imagMatrix = new float[menge][N]; //
+		
+		calculateXfd(realXfd, imagXfd); // Matrix Xfd
+		
+		calculateDft(realCode, imagCode, realDftCode, imagDftCode); // Array DFT(C)
+		changeSignalElements(imagDftCode); // Calculate complex conjugate of elements of DFT(C)
+		
+		calculateArgumentOfIdft(realXfd, imagXfd, realDftCode, imagDftCode, realMatrix, imagMatrix); // Array DFT(Xfd)*complexConj(DFT(C))
+																									 // for each fd
+		
 		
 		
 		Pin = calculateInputSignalEstimation();
@@ -99,20 +101,77 @@ public class Acquisition {
 		return Pin;
 	}
 	
-	private float calculateRealEulerFactors(int fd, int nSample){
+	/****************  functions for calculation of matrix Xfd  *********************/
+	private void calculateXfd(float[][] realXfd, float[][] imagXfd){
+		int n, fd, m = 0;
 		
-		double arg = (fd*nSample*2*pi)/sampleFrequenz;
+		for(fd = minFrequenz; fd <= maxFrequenz; fd += stepFrequenz){
+			for(n = 0; n < N; n++){
+				float cosx = calculateCosx(fd*n, sampleFrequenz); // e^(-j*2pi*fd*n/fs) = cos(-2pi*fd*n/fs) + jsen(-2pi*fd*n*fs)
+				float sinx = calculateSinx(fd*n, sampleFrequenz); //
+				
+				realXfd[m][n] = realSample[n]*cosx - imagSample[n]*sinx;
+				imagXfd[m][n] = realSample[n]*sinx + imagSample[n]*cosx;
+			}
+			m++;
+		}
+	}
+	
+	private float calculateCosx(int mult, int div){
+		
+		float arg = -(2*pi*mult)/div;
 		float realEuler = (float) Math.cos(arg);
 		
 		return realEuler;
 	}
 	
-	private float calculateImagEulerFactors(int fd, int nSample){
+	private float calculateSinx(int mult, int div){
 		
-		double arg = (fd*nSample*2*pi)/sampleFrequenz;
+		double arg = -(2*pi*mult)/div;
 		float imagEuler = (float) Math.sin(arg);
 		
 		return imagEuler;
+	}
+	/****************  END functions for calculation of matrix Xfd  *******************/
+	
+	private void calculateDft(float[] realInput, float[] imagInput, float[] realOutput, float[] imagOutput){
+		int n = realInput.length;
+		for(int k = 0; k < n; k++){
+			float sumReal = 0;
+			float sumImag = 0;
+			for(int t = 0; t < n; t++){  // For each input element
+				float cosx = calculateCosx(t*k, n);
+				float sinx = calculateSinx(t*k, n);
+				sumReal += realInput[t]*cosx - imagInput[t]*sinx;
+				sumImag += realInput[t]*sinx + imagInput[t]*cosx;
+			}
+			realOutput[k] = sumReal;
+			imagOutput[k] = sumImag;
+		}
+	}
+	
+	private void changeSignalElements(float[] input){
+		int n = input.length;
+		for(int i = 0; i < n; i++){
+			input[i] = -input[i];
+		}
+	}
+	
+	private void calculateArgumentOfIdft(float[][] realXfd, float[][] imagXfd, float[] realDftCode, 
+										 float[] imagDftCode, float[][] realOutput, float[][] imagOutput){
+		
+		int menge = calculateMengeFrequenzen();
+		for(int m = 0; m < menge; m++){ // For each fd
+			calculateDft(realXfd[m], imagXfd[m], realOutput[m], imagOutput[m]);  // Array DFT(Xfd) for particular fd
+			
+			for(int n = 0; n < N; n++){
+				float realElementXfd = realOutput[m][n];
+				float imagElementXfd = imagOutput[m][n];
+				
+				realOutput[m][n] = realElementXfd*realDftCode[n] - imagElementXfd*imagDftCode[n]; // Array DFT(Xfd)*complexConj(DFT(C))
+				imagOutput[m][n] = realElementXfd*imagDftCode[n] + imagElementXfd*realDftCode[n]; // for particular fd
+			}
+		}
 	}
 
 }
